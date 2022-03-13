5e868a83ce8a6e4ca1cee93bd6abb5aa0ab649c0
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-03-13 15:56:07 +0200

    added changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,16 @@
-import logo_react from '../img/reactjs-icon_2.png'
-export default function  Main() {
-	return (
-		<main className="main">
-			<img className='main__react-logo' src={logo_react} alt='logo-react'/>
-			<h1 className="main__title">Fun facts about React</h1>
-			<ul className="main__list"> 
-				<li className="main__list-item">Was first released in 2013</li>
-				<li className="main__list-item">Was originally created by Jordan Walke</li>
-				<li className="main__list-item">Has well over 100K stars on GitHub</li>
-				<li className="main__list-item">Is maintained by Facebook</li>
-				<li className="main__list-item">Powers thousands of enterprise apps, including mobile apps</li>
-			</ul>
-		</main>
-	)
+import React from "react"
+
+export default function Main(props) {
+    return (
+        <main className={props.darkMode ? "dark" : ""}>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,28 @@
-import logo from '../img/reactjs-icon.svg'
-export default function  Navbar() {
-	return (
-		<nav className='navbar'>
-			<img className='navbar-img' src={logo} alt='logo'/>
-			<h2 className='navbar-header'>ReactFacts</h2>
-			<h3 className='navbar-name'>React Course - Project 1</h3>
-		</nav>
-	)
+import React from "react"
+
+export default function Navbar(props) {
+    return (
+         <nav 
+            className={props.darkMode ? "dark": ""}
+        >
+            <img 
+                className="nav--logo_icon"
+                src="./images/react-icon-small.png"
+            />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
+    )
 }
